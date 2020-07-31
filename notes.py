3aaaf6a7fac397cd3984da7a7a790effc9dd52da
Robert Fuchs <robert.fuchs@logmein.com>
2020-07-31 11:52:30 +0200

    Create level one index hierarchy

--- notes.py
+++ notes.py
@@ -1,10 +1,39 @@
 import subprocess
 import click
+import os
+import pathlib
 
 @click.command()
 def notes():
     """Note taking CLI."""
+    index_notes()
     subprocess.Popen(["vmd", "index.md"])
+
+
+def index_notes():
+    with open('index.md', 'w') as index:
+        index.write("# Index\n")
+
+        for root, dirs, files in os.walk('.'):
+            if 'venv' in dirs:
+                dirs.remove('venv')
+            if '.vscode' in dirs:
+                dirs.remove('.vscode')
+            if '.git' in dirs:
+                dirs.remove('.git')
+
+
+            if os.path.basename(root) != '.':
+                index.write(f"## {os.path.basename(root)}\n")
+
+            for file in files:
+                name, extension = file.split('.')
+                if extension != 'md':
+                    continue
+
+                index.write(f"- [{name}]({root}/{file})\n")
+
+            
 
 if __name__ == '__main__':
     notes()
