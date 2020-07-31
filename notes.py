8fefc4a4a36824e084f00f3b00979aa2e28f97e2
Robert Fuchs <robert.fuchs@logmein.com>
2020-07-31 12:09:42 +0200

    Refactor code

--- notes.py
+++ notes.py
@@ -3,14 +3,16 @@ import click
 import os
 import pathlib
 
+MARKDOWN_VIEWER = "vmd"
+
 @click.command()
 def notes():
     """Note taking CLI."""
-    index_notes()
-    subprocess.Popen(["vmd", "index.md"])
+    create_index()
+    subprocess.Popen([MARKDOWN_VIEWER, "index.md"])
 
 
-def index_notes():
+def create_index():
     with open('index.md', 'w') as index:
         index.write("# Index\n")
 
@@ -25,13 +27,15 @@ def index_notes():
             depth = root.count('/') + 1
 
             if os.path.basename(root) != '.':
+                # Don't list root directoy as heading
                 index.write(f"{'#' * depth} {os.path.basename(root)}\n")
 
             for file in files:
                 name, extension = file.split('.')
                 if extension != 'md':
                     continue
-
+                
+                name = name.replace('-', ' ').title()
                 index.write(f"- [{name}]({root}/{file})\n")
 
 
