2ac08dbcc3ab265aaf5f7077cd551972afe7b0b7
Robert Fuchs <robert.fuchs@logmein.com>
2020-08-07 12:27:43 +0200

    Add edit command and refactor code

--- notes.py
+++ notes.py
@@ -3,32 +3,55 @@ import click
 import os
 import pathlib
 
+from rich.console import Console
+from rich.markdown import Markdown
+
 MARKDOWN_VIEWER = "vmd"
 
+
 @click.group(invoke_without_command=True)
-def cli():
+@click.pass_context
+def cli(ctx):
     """Note taking CLI."""
     create_index()
-    subprocess.Popen([MARKDOWN_VIEWER, "index.md"])
+
+    if ctx.invoked_subcommand is None:
+        # Only open index if invoked without any subcommand
+        subprocess.Popen([MARKDOWN_VIEWER, "index.md"])
 
 
 def create_index():
-    with open('index.md', 'w') as index:
-        index.write("# Index\n")
+    with open('index.md', 'w') as index_file:
+        index_file.write("# Index\n")
 
         for root, _, files in os.walk('.'):
             depth = root.count('/') + 1
 
             if os.path.basename(root) != '.':
                 # Don't list root directoy as heading
-                index.write(f"{'#' * depth} {os.path.basename(root)}\n")
+                # Write categories as titles
+                index_file.write(f"{'#' * depth} {os.path.basename(root)}\n")
 
-            for file in files:
-                try:
-                    name, extension = file.split('.')
+            write_list_of_notes(files, index_file, root)
 
-                    if extension == 'md':
-                        name = name.replace('-', ' ').title()
-                        index.write(f"- [{name}]({root}/{file})\n")
-                except:
-                    continue
+
+def write_list_of_notes(files, index_file, root):
+    for file in files:
+        try:
+            name, extension = file.split('.')
+
+            if extension == 'md':
+                # Only list markdown files
+                name = name.replace('-', ' ').title()
+                index_file.write(f"- [{name}]({root}/{file})\n")
+        except:
+            continue
+
+
+@click.command()
+@click.argument('file_name')
+def edit(file_name):
+    subprocess.run([os.environ['EDITOR'], file_name])
+
+
+cli.add_command(edit)
